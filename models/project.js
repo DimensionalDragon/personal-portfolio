4ec82789c7bdffe7e47e062b72c88c27556e73db
DimensionalDragon <andhikangurah@gmail.com>
2022-01-09 04:39:19 +0800

    Initial commit

---
+++ models/project.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model('Project', projectSchema);
---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+const {DB_URI} = process.env;
+
+mongoose.connect(DB_URI);
+const db = mongoose.connection;
+db.on('error', (err) => console.error('[Error]', err));
+db.once('open', () => console.log('Connected to database'));
+
+const Project = require('../models/project');
+
+app.use(cors());
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+    try {
+        const projects = await Project.find().sort({ _id: -1 }).limit(6);
+        console.log(projects);
+        res.render('index', {pageTitle: 'DimensionalDragon', projects});
+    } catch(err) {
+        return res.status(500).json({ok: false, message: err.message});
+    }
+});
+
+app.get('/projects', (req, res) => {
+    res.send('<h1>Projects Page</h1>');
+});
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
